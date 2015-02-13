5012f73c77ef52dc38545a50d242131993cbba12
Noah Kochanowicz <noah.kochanowicz@agapered.com>
2015-02-13 17:09:34 -0500

    Fixes test controllers

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-  before_filter :api_authenticate
+  before_filter :restrict_to_api_users
   def index
     render json: {success: true, word: "The Bird"}
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
+  resources :words
   devise_for :users
 
   # Example of regular route:
