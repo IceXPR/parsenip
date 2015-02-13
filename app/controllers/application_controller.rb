9a4253f825f7794025b4e8c058a5f2056b29d8cf
Noah Kochanowicz <noah.kochanowicz@agapered.com>
2015-02-13 15:24:34 -0500

    Adds basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :handle_invalid_request
+
+  def handle_invalid_request
+    if request.xhr? && !api_authenticate
+      render json: {success: false, error: I18n.t('errors.not_permitted')} 
+    end
+  end
+
+  def restrict_to_xhr
+    if !request.xhr?
+      raise "Forbidden" 
+    end
+  end
+
+  private
+
+  def api_authenticate
+    @user = User.find_by_id(ApiAuth.access_id(request))
+    return ApiAuth.authentic?(request, @user.secret_key) unless @user.nil?
+    false
+  end
 end
