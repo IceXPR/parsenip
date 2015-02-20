98ca2cd4997585d8ce861dc3fda74484ff1bf66d
Jacob Lee <jacob.lee@agapered.com>
2015-02-20 21:02:46 +0000

    Authenticates with user's api key

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,10 +1,11 @@
 class UploadsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_filter :restrict_to_api_users
 
 
   def create
-    puts params
     upload = Upload.new
+    upload.user = @user
     upload.file = params['file']
     if upload.save
       render json: {success: 'true', upload_token: "#{upload.upload_token}"}
@@ -12,10 +13,13 @@ class UploadsController < ApplicationController
   end
 
   def progress
-    puts params
     if params['upload_token']
-      upload = Upload.find_by(upload_token: params['upload_token'])
-      render json: {progress: "#{upload.percent_complete}"}
+      upload = @user.uploads.find_by(upload_token: params['upload_token'])
+      if upload
+        render json: {progress: "#{upload.percent_complete}"}
+      else
+        render json: {error: 'No upload matches your token and user'}
+      end
     else
       render json: {error: 'Upload Token Required'}
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   before_create :assign_secret_key
+  has_many :uploads
  
   private
  
