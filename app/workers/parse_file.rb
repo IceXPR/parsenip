e0fb8ed14e177d0a64ca955c0e267810db56bd8f
Michael Patterson <michael.patterson@agapered.com>
2015-02-20 16:59:22 -0600

    Save the data in the correct records.

--- app/workers/parse_file.rb
+++ app/workers/parse_file.rb
@@ -4,9 +4,12 @@ class ParseFile
   include Sidekiq::Worker
   def perform(upload_id)
     upload = Upload.find(upload_id)
+    # TODO: raise error if upload already has parse_data
+    # raise "Upload #{upload.id} already processed" if upload.parse_data.count > 0
+    # TODO: remove this:
+    upload.parse_data.destroy_all
+
     headers = ColumnMatchService.new(upload.file).detect
-
-
-    binding.pry
+    DataGrabberService.new(headers, upload).start
   end
 end
