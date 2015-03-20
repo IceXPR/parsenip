1222a30e85526f0098011115fcaba0ce8b0e8c46
Michael Patterson <michael.patterson@agapered.com>
2015-03-20 14:27:19 -0500

    Only run resque web in development.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :api_keys
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+
+  if Rails.env.development?
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
