337e479705da5f1f57d673d4020ff5e3d0851701
Noah Kochanowicz <noah.kochanowicz@agapered.com>
2015-03-27 20:54:13 +0000

    break return at correct spot

--- lib/parsenip/detection/dictionary.rb
+++ lib/parsenip/detection/dictionary.rb
@@ -1,4 +1,3 @@
-
 module Parsenip
   module Detection
     class Dictionary < Parsenip::Detection::Column
@@ -12,6 +11,7 @@ module Parsenip
         @options = options
         @ticker = {}
       end
+
       def match
         update_rate = 10
         chunk_size = Upload.uploader_chunk_size 
@@ -28,9 +28,9 @@ module Parsenip
               break
               return
             end
-            break
-            return
           end
+          break
+          return
           puts "Ticker #{@ticker.inspect}"
         end
         puts "Ticker #{@ticker.inspect}"
