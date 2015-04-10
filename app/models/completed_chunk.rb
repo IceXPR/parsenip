13d78aaa85d461e60965d77716a64df9fcffed42
Jacob Lee <jacob.lee@agapered.com>
2015-04-10 20:23:21 +0000

    Spins out second round of parsing into individual workers. Greatly improves speed

---
+++ app/models/completed_chunk.rb
@@ -0,0 +1,5 @@
+class CompletedChunk < ActiveRecord::Base
+
+  belongs_to :upload
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403205943) do
+ActiveRecord::Schema.define(version: 20150410200727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20150403205943) do
   end
 
   add_index "api_keys", ["user_id"], name: "index_api_keys_on_user_id", using: :btree
+
+  create_table "completed_chunks", force: :cascade do |t|
+    t.integer  "upload_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dictionaries", force: :cascade do |t|
     t.string   "value"
