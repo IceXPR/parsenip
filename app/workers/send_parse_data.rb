e102f68f47a9746e18f9eb439eaafe8453ea1add
Michael Patterson <michael.patterson@agapered.com>
2015-06-25 13:52:55 -0400

    Only attempt to send parse data if it has not been sent.

--- app/workers/send_parse_data.rb
+++ app/workers/send_parse_data.rb
@@ -5,6 +5,6 @@ class SendParseData
   include Sidekiq::Worker
   def perform(upload_id)
     upload = Upload.find(upload_id)
-    upload.send_parse_data!
+    upload.send_parse_data! unless upload.data_sent
   end
 end
